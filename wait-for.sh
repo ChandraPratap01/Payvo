a4003da7e99ffa6e54ad3913ad766eaf4364067c
Chandra Chauhan <chandrapratap0001@gmail.com>
2025-04-08 02:41:49 +0530

    addedWait_for_sh

---
+++ wait-for.sh
@@ -0,0 +1,14 @@
+
+host="$1"
+port="$2"
+timeout="${3:-30}"
+
+echo "Waiting for $host:$port to be ready..."
+
+for i in $(seq 1 "$timeout"); do
+  nc -z "$host" "$port" && echo "$host:$port is ready!" && exit 0
+  sleep 1
+done
+
+echo "Timeout while waiting for $host:$port"
+exit 1
